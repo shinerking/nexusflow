0a9b0dee7372e2aaf1c5196837bfb69c53f7c1a9
shinerking <abimanyuriantoputra@gmail.com>
2026-02-08 13:45:15 +0700

    Enable Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Toaster position="top-right" richColors closeButton />
+        <SpeedInsights />
       </body>
     </html>
   );
